196011b3799c058e838c0634525e3cae202e9e08
Marco Aurélio <marco.hub21@gmail.com>
2023-11-08 20:39:47 -0300

    adiciona sessão de projetos

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,18 @@ const App = () => {
   const [selectedTecnology, setSelectedTecnology] = useState("");
 
   const tecnologias = [
-    "html",
-    "css",
-    "php",
-    "javascript",
-    "typescript",
-    "python",
-    "tailwind",
-    "react",
-    "laravel",
-    "mysql",
-    "git",
-    "figma",
+    { nome: "html", descricao: "descrição html" },
+    { nome: "css", descricao: "descrição css" },
+    { nome: "php", descricao: "descrição php" },
+    { nome: "javascript", descricao: "descrição javascript" },
+    { nome: "typescript", descricao: "descrição typescript" },
+    { nome: "python", descricao: "descrição python" },
+    { nome: "tailwind", descricao: "descrição tailwind" },
+    { nome: "react", descricao: "descrição react" },
+    { nome: "laravel", descricao: "descrição laravel" },
+    { nome: "mysql", descricao: "descrição mysql" },
+    { nome: "git", descricao: "descrição git" },
+    { nome: "figma", descricao: "descrição figma" },
   ];
 
   useEffect(() => {
@@ -52,9 +52,9 @@ const App = () => {
 
   return (
     <div className="h-screen">
-      <header className="fixed z-10 flex justify-around items-center text-5xl w-full h-20 bg-gray-950/80 border-b-2 border-[#111111] ">
+      <header className="fixed z-10 flex justify-around items-center text-5xl w-full h-20 bg-background3/80 border-b-2 border-background3 ">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <h2 className="ml-3 text-4xl text-[#64ffda] font-[Anurati]">MARCO</h2>
+          <h2 className="ml-3 text-4xl text-primary font-[Anurati]">MARCO</h2>
           <menu className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <a
               className={`text-lg mr-6 transition duration-300 ease-in-out hover:text-white ${
@@ -92,11 +92,11 @@ const App = () => {
         </div>
       </header>
 
-      <section id="Início" className="section bg-[#0a192f] pt-20">
+      <section id="Início" className="section bg-background2 pt-20">
         <main className="w-full h-screen">
           <div className="w-full h-full flex flex-col-reverse items-center justify-between md:grid md:grid-cols-2 px-5 md:px-48">
             <div className="py-5">
-              <h3 className="text-2xl mb-2 text-[#64ffda]">Olá, meu nome é</h3>
+              <h3 className="text-2xl mb-2 text-primary">Olá, meu nome é</h3>
               <h2 className="text-6xl mb-6 text-white/90 font-bold">Marco Pitanga.</h2>
               <p className="text-4xl text-white/60 font-medium">
                 Sou um desenvolvedor especializado em desenvolver aplicações Web.
@@ -110,10 +110,10 @@ const App = () => {
         </main>
       </section>
 
-      <section id="Sobre mim" className="section bg-[#0a192f]">
-        <div className="flex justify-between items-center p-6  border-2 border-gray-800 bg-gray-950 md:grid md:grid-cols-2 max-w-[1140px] my-0 mx-auto">
+      <section id="Sobre mim" className="section bg-background">
+        <div className="flex justify-between items-center p-6  border-2 border-gray-800 bg-background3 md:grid md:grid-cols-2 max-w-[1140px] my-0 mx-auto">
           <div className="flex justify-center items-center h-[500px]">
-            <div className="h-[300px] w-[300px] bg-[#64ffda] rounded-full flex justify-center items-center uppercase text-9xl text-gray-950 font-[Anurati]">
+            <div className="h-[300px] w-[300px] bg-primary rounded-full flex justify-center items-center uppercase text-9xl text-gray-950 font-[Anurati]">
               <p>mp</p>
             </div>
           </div>
@@ -121,7 +121,7 @@ const App = () => {
             <div className="row-span-1"></div>
             <div className="row-span-3">
               <h2 className="text-4xl mb-3 text-white">Marco Pitanga</h2>
-              <h3 className="text-xl mb-6 text-[#64ffda]">Desenvolvedor Web</h3>
+              <h3 className="text-xl mb-6 text-primary">Desenvolvedor Web</h3>
               <p className="text-xl text-justify">
                 Sou estudante de Ciência da Computação, focado em Desenvolvimento Web há 3 anos.
                 Profissionalmente tenho experiência com estagio full-stack, contribuindo para o
@@ -137,6 +137,7 @@ const App = () => {
                 bgColor="#0a192f"
                 url="https://github.com/MarcoPitanga"
                 target="_blank"
+                className="hover:border-2 hover:border-primary rounded-full"
               />
               <SocialIcon
                 data-tooltip-id="linkedin"
@@ -145,6 +146,7 @@ const App = () => {
                 bgColor="#0a192f"
                 url="https://www.linkedin.com/in/marco-pitanga/"
                 target="_blank"
+                className="hover:border-2 hover:border-primary rounded-full"
               />
               <SocialIcon
                 data-tooltip-id="instagram"
@@ -153,15 +155,16 @@ const App = () => {
                 bgColor="#0a192f"
                 url="https://www.instagram.com/marco.215/"
                 target="_blank"
+                className="hover:border-2 hover:border-primary rounded-full"
               />
               <SocialIcon
                 data-tooltip-id="email"
                 network="email"
                 fgColor="#64ffda"
                 bgColor="#0a192f"
-                className="hover:cursor-pointer"
                 onClick={() => (window.location.href = "mailto:marcopitanga21@gmail.com")}
                 target="_blank"
+                className="hover:cursor-pointer hover:border-2 hover:border-primary rounded-full"
               />
               <SocialIcon
                 data-tooltip-id="number"
@@ -170,6 +173,7 @@ const App = () => {
                 bgColor="#0a192f"
                 url="https://wa.me/5521983440206/"
                 target="_blank"
+                className="hover:border-2 hover:border-primary rounded-full"
               />
               <ReactTooltip
                 id="github"
@@ -206,7 +210,7 @@ const App = () => {
         </div>
       </section>
 
-      <section id="Conhecimentos" className="section bg-[#0a192f]">
+      <section id="Conhecimentos" className="section bg-background2">
         <div className="flex justify-between items-center p-6 md:grid md:grid-cols-2 max-w-[1140px] my-0 mx-auto">
           <div className="flex flex-col w-[500px] h-[500px] max-w-[500px]">
             <h3 className="font-semibold text-4xl mb-4 text-white">Conhecimentos</h3>
@@ -219,20 +223,54 @@ const App = () => {
               <div
                 key={index}
                 tabIndex={1}
-                onMouseEnter={() => handleSelectedTecnology(tecnologia)}
+                onMouseEnter={() => handleSelectedTecnology(tecnologia.descricao)}
                 onMouseLeave={() => handleSelectedTecnology("")}
                 role="button"
-                className="border border-[#0a192f] hover:border-[#64ffda] duration-500 rounded-xl bg-gray-950/25 flex justify-center items-center"
+                className="border border-background hover:border-primary duration-500 rounded-xl bg-background3/25 flex justify-center items-center"
               >
-                <img src={`logo-${tecnologia}.svg`} width={75} />
+                <img src={`logo-${tecnologia.nome}.svg`} width={75} />
               </div>
             ))}
           </div>
         </div>
       </section>
 
-      <section id="Projetos" className="section bg-[#0a192f]">
-        Em breve
+      <section id="Projetos" className="section bg-background">
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-white font-semibold text-4xl mb-20">Projetos</h2>
+          <div className="h-full grid grid-cols-4 gap-4">
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((tecnologia, index) => (
+              <div
+                key={index}
+                tabIndex={1}
+                role="button"
+                className="border border-background px-3 pt-3 duration-500 rounded-xl bg-background3/25 flex flex-col justify-center items-center"
+              >
+                <div className="w-[200px] h-[200px] border-b border-primary">
+                  <h3 className="text-2xl text-center text-white">Titulo</h3>
+                  <p className="text-lg">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit, numquam temporibus
+                    voluptas.
+                  </p>
+                </div>
+                <div className="flex items-center justify-center gap-4 p-2">
+                  <div className="w-11 h-11 border-2 border-transparent hover:border-primary rounded-full text-xl hover:text-lg flex justify-center items-center p-6 bg-background text-primary">
+                    site
+                  </div>
+                  <SocialIcon
+                    data-tooltip-id="github"
+                    network="github"
+                    fgColor="#64ffda"
+                    bgColor="#0a192f"
+                    url="https://github.com/MarcoPitanga"
+                    target="_blank"
+                    className="hover:border-2 hover:border-primary rounded-full"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#64ffda",
+        background: "#0a192f",
+        background2: "#081020",
+        background3: "#030712",
+      },
+    },
   },
   plugins: [],
 };
