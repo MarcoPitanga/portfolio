e313fc8e381b0f34e19f8fe1447cba118263b9a6
Marco Aurélio <marco.hub21@gmail.com>
2024-01-27 19:13:46 -0300

    torna a sessão "sobre mim" responsiva

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,10 @@ const App = () => {
     <div className="h-screen scrollbar">
       <Navbar items={["Início", "Sobre mim", "Conhecimentos", "Projetos"]} />
 
-      <Section name="Início" className="bg-background2 bg-texture bg-fixed">
+      <Section name="Início" className="bg-background2 bg-texture bg-fixed h-auto">
         <main className="w-full h-screen">
           <div className="w-full h-full flex flex-col-reverse items-center justify-between lg:grid lg:grid-cols-2 px-5 lg:px-48">
-            <div className="py-5">
+            <div className="py-5 hidden md:block">
               <h3 className="text-2xl mb-2 text-primary text-center lg:text-start">
                 Olá, meu nome é
               </h3>
@@ -30,6 +30,94 @@ const App = () => {
               </p>
             </div>
 
+            <div className="py-5 block md:hidden">
+              <h2 className="text-4xl mb-3 text-white">Marco Pitanga</h2>
+              <h3 className="text-xl mb-6 text-primary">Desenvolvedor Web</h3>
+              <p className="text-xl text-justify">
+                Recém-formado em Ciência da Computação, com 3 anos de foco em Desenvolvimento Web.
+                Contribuí como estagiário full-stack para o desenvolvimento de uma plataforma
+                interna de gestão em um estúdio criativo. Agora, estou buscando oportunidades para
+                aplicar e expandir minhas habilidades no desenvolvimento web.
+              </p>
+              <div className="flex justify-center row-span-1 gap-5 mt-10">
+                <SocialIcon
+                  data-tooltip-id="github"
+                  network="github"
+                  fgColor="#64ffda"
+                  bgColor="#0a192f"
+                  url="https://github.com/MarcoPitanga"
+                  target="_blank"
+                  className="hover:border-2 hover:border-primary rounded-full"
+                />
+                <SocialIcon
+                  data-tooltip-id="linkedin"
+                  network="linkedin"
+                  fgColor="#64ffda"
+                  bgColor="#0a192f"
+                  url="https://www.linkedin.com/in/marco-pitanga/"
+                  target="_blank"
+                  className="hover:border-2 hover:border-primary rounded-full"
+                />
+                <SocialIcon
+                  data-tooltip-id="instagram"
+                  network="instagram"
+                  fgColor="#64ffda"
+                  bgColor="#0a192f"
+                  url="https://www.instagram.com/marco.215/"
+                  target="_blank"
+                  className="hover:border-2 hover:border-primary rounded-full"
+                />
+                <SocialIcon
+                  data-tooltip-id="email"
+                  network="email"
+                  fgColor="#64ffda"
+                  bgColor="#0a192f"
+                  onClick={() => (window.location.href = "mailto:marcopitanga21@gmail.com")}
+                  target="_blank"
+                  className="hover:cursor-pointer hover:border-2 hover:border-primary rounded-full"
+                />
+                <SocialIcon
+                  data-tooltip-id="number"
+                  network="whatsapp"
+                  fgColor="#64ffda"
+                  bgColor="#0a192f"
+                  url="https://wa.me/5521983440206/"
+                  target="_blank"
+                  className="hover:border-2 hover:border-primary rounded-full"
+                />
+                <ReactTooltip
+                  id="github"
+                  place="bottom"
+                  style={{ fontSize: "14px" }}
+                  content="MarcoPitanga"
+                />
+                <ReactTooltip
+                  id="linkedin"
+                  place="bottom"
+                  style={{ fontSize: "14px" }}
+                  content="marco-pitanga"
+                />
+                <ReactTooltip
+                  id="instagram"
+                  place="bottom"
+                  style={{ fontSize: "14px" }}
+                  content="marco.215"
+                />
+                <ReactTooltip
+                  id="number"
+                  place="bottom"
+                  style={{ fontSize: "14px" }}
+                  content="(21) 98344-0206"
+                />
+                <ReactTooltip
+                  id="email"
+                  place="bottom"
+                  style={{ fontSize: "14px" }}
+                  content="marcopitanga21@gmail.com"
+                />
+              </div>
+            </div>
+
             <aside className="w-full h-full flex flex-col items-center lg:items-end justify-center">
               <img src="aside-main.png" className="aside-main" />
             </aside>
@@ -37,7 +125,7 @@ const App = () => {
         </main>
       </Section>
 
-      <Section name="Sobre mim" className="bg-background2 bg-texture bg-fixed">
+      <Section name="Sobre mim" className="hidden md:block bg-background2 bg-texture bg-fixed">
         <div className="flex justify-between items-center p-6  border-2 border-gray-800 bg-background3 md:grid md:grid-cols-2 max-w-[1140px] my-0 mx-auto">
           <div className="flex justify-center items-center h-[500px]">
             <div className="h-[300px] w-[300px] bg-primary rounded-full flex justify-center items-center uppercase text-9xl text-gray-950 font-[Anurati]">
